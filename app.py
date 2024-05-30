13d2ec6320dcb5583d13f3e543daa74152d18a94
Marvellous Alabi <60781267+marvellousadeoluwa@users.noreply.github.com>
2024-05-30 02:51:11 +0100

    Update app.py
    model directory changes

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ __version__ = '0.1.0'
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
 
-with open(f'{BASE_DIR}/trained_pipeline-{__version__}.pkl', 'rb') as f:
+with open(f'trained_pipeline-{__version__}.pkl', 'rb') as f:
     model = pickle.load(f)
-
+    #f'{BASE_DIR}/trained_pipeline-{__version__}.pkl'
 # Model and prediction
 classes = [
     'Arabic',
